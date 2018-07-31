a68c577c826cc2c3e482bd129920f22e3f392581
Mark Dewey <mark.t.dewey@gmail.com>
2018-07-31 11:19:10 -0400

    first pass at gen a random number

--- if-then-demo/public/main.js
+++ if-then-demo/public/main.js
@@ -1,14 +1,16 @@
-// const main = () => {
-//   document.querySelector('h1').textContent += '?'
-// }
+let randomPick = 0
 
-// document.addEventListener('DOMContentLoaded', main)
+const init = () => {
+  // pick a random number between 1 and 10
+  randomPick = Math.ceil(Math.random() * 10)
+  console.log('the current number is ', randomPick)
+}
 
 const updatePlayerName = () => {
   console.log('updating player name')
   const playerName = document.querySelector('.player-name-input').value
   document.querySelector('#playerName').textContent = playerName
-
 }
 
+document.addEventListener('DOMContentLoaded', init)
 document.querySelector('.player-name-input').onchange = updatePlayerName
