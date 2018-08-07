a7eaf9663aeba0d0244a7b0a30f2657c73092dc4
Mark Dewey <mark.t.dewey@gmail.com>
2018-08-07 12:14:50 -0400

    added star wars api

--- star-wars/public/main.js
+++ star-wars/public/main.js
@@ -22,7 +22,16 @@ const searchAPI = () => {
       const parent = document.querySelector('.searchResults')
       searchResults.results.forEach(person => {
         const li = document.createElement('li')
-        li.textContent = person.name
+        
+        const _name = document.createElement('h4')
+        _name.textContent = person.name
+        
+        const _eyeColor = document.createElement('h6')
+        _eyeColor.textContent = "eyes:" + person.eye_color
+        li.appendChild(_name).appendChild(_eyeColor)
+        
+        const _hairColor  = person.hair_color.split(',').pop().trim()
+        li.classList.add(_hairColor)
         parent.appendChild(li)
       });
     })
