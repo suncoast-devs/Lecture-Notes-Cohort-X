53769bff3045fa34f30f841e2d9afe0a2cde4f85
Mark Dewey <mark.t.dewey@gmail.com>
2018-08-07 11:43:12 -0400

    basic search is working

--- star-wars/README.md
+++ star-wars/README.md
@@ -1,1 +1,7 @@
-# Star Wars
+# Star Wars API Explorer
+
+
+## Requirements
+
+- [ ] search for a person
+- [ ] view all details for a selected person
--- star-wars/public/main.js
+++ star-wars/public/main.js
@@ -2,4 +2,31 @@ const main = () => {
   document.querySelector('h1').textContent += '?'
 }
 
+const searchAPI = () => {
+  const userInput = document.querySelector('.input').value
+
+  console.log(userInput)
+
+  const _url = 'https://swapi.co/api/people/?search=' + userInput
+
+  fetch(_url)
+    .then(resp => {
+      console.log("response = ", resp)
+      if ( resp.status === 200){
+        return resp.json()
+      } else {
+        console.log("err", resp)
+      }
+    }).then(searchResults => {
+      console.log("search results = ", searchResults)
+      const parent = document.querySelector('.searchResults')
+      searchResults.results.forEach(person => {
+        const li = document.createElement('li')
+        li.textContent = person.name
+        parent.appendChild(li)
+      });
+    })
+}
+
 document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.input').addEventListener('blur', searchAPI)
