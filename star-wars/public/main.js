a52d06e375ef40a6c42d64c78809392db2bc5ffa
Mark Dewey <mark.t.dewey@gmail.com>
2018-08-08 11:22:13 -0400

    refactored to use class for search

--- star-wars/public/main.js
+++ star-wars/public/main.js
@@ -1,41 +1,43 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
+class Search {
+  constructor(searchTerm) {
+    this.searchTerm = searchTerm
+    this.searchUrl = 'https://swapi.co/api/people/?search=' + searchTerm
+  }
+
+  getSearchResults() {
+    fetch(this.searchUrl)
+      .then(resp => {
+        console.log("response = ", resp)
+        if (resp.status === 200) {
+          return resp.json()
+        } else {
+          console.log("err", resp)
+        }
+      }).then(searchResults => {
+        console.log("search results = ", searchResults)
+        const parent = document.querySelector('.searchResults')
+        searchResults.results.forEach(person => {
+          const li = document.createElement('li')
+
+          const _name = document.createElement('h4')
+          _name.textContent = person.name
+
+          const _eyeColor = document.createElement('h6')
+          _eyeColor.textContent = "eyes:" + person.eye_color
+          li.appendChild(_name).appendChild(_eyeColor)
+
+          const _hairColor = person.hair_color.split(',').pop().trim()
+          li.classList.add(_hairColor)
+          parent.appendChild(li)
+        });
+      })
+  }
 }
 
-const searchAPI = () => {
+const searchAPIEvent = () => {
   const userInput = document.querySelector('.input').value
-
-  console.log(userInput)
-
-  const _url = 'https://swapi.co/api/people/?search=' + userInput
-
-  fetch(_url)
-    .then(resp => {
-      console.log("response = ", resp)
-      if ( resp.status === 200){
-        return resp.json()
-      } else {
-        console.log("err", resp)
-      }
-    }).then(searchResults => {
-      console.log("search results = ", searchResults)
-      const parent = document.querySelector('.searchResults')
-      searchResults.results.forEach(person => {
-        const li = document.createElement('li')
-        
-        const _name = document.createElement('h4')
-        _name.textContent = person.name
-        
-        const _eyeColor = document.createElement('h6')
-        _eyeColor.textContent = "eyes:" + person.eye_color
-        li.appendChild(_name).appendChild(_eyeColor)
-        
-        const _hairColor  = person.hair_color.split(',').pop().trim()
-        li.classList.add(_hairColor)
-        parent.appendChild(li)
-      });
-    })
+  const apiSearch = new Search(userInput)
+  apiSearch.getSearchResults()
 }
 
-document.addEventListener('DOMContentLoaded', main)
-document.querySelector('.input').addEventListener('blur', searchAPI)
+document.querySelector('.input').addEventListener('blur', searchAPIEvent)
