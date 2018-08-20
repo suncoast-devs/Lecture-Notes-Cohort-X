8f4b3dd5fdf3bfd38b9602cd59fdb4d30ca5900c
Mark Dewey <mark.t.dewey@gmail.com>
2018-08-20 10:52:14 -0400

    creating board and showing board

---
+++ minesweeper2/src/Minesweeper.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+const BASE_URL = 'https://minesweeper-api.herokuapp.com/'
+
+class Minesweeper extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            game:{
+                board:[]
+            }
+        }
+    }
+    
+
+    componentDidMount () {
+        fetch(`${BASE_URL}games`, {
+            method:"POST",
+            body:JSON.stringify({difficulty:0}),
+            headers:{
+                'Content-Type':'application/json'
+            }
+        })
+        .then(resp => resp.json())
+        .then(newGame => {
+            console.log(newGame)
+            this.setState({
+                game:newGame
+            })
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <table>
+                    <tbody>
+                      {this.state.game.board.map((row, i) => {
+                         return  <tr key={i}>{
+                             row.map((col, j) => {
+                                 return <td key={j} className="cell">{this.state.game.board[i][j]}</td>
+                             })
+                         }</tr>
+                      })}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+export default Minesweeper;
