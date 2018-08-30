64469438e931042bdf466ff8b8dcf81a7631182f
Mark Dewey <mark.t.dewey@gmail.com>
2018-08-30 08:40:56 -0400

    updated scoreboard

--- scoreboard-react-app/src/App.js
+++ scoreboard-react-app/src/App.js
@@ -4,16 +4,27 @@ import './App.css';
 import TeamScoreBoard from './TeamScoreBoard'
 
 class App extends Component {
+
+  resetScore = () => {
+    localStorage.setItem("Home-team-score", 0)
+    
+    localStorage.removeItem("Away-team-score")
+    // localStorage.clear()
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">My Cool Scoreboard</h1>
         </header>
-        <section>
-          <TeamScoreBoard />
-          <TeamScoreBoard />
+        <section className="boards">
+          <TeamScoreBoard teamName={"Home"} />
+          <TeamScoreBoard teamName={"Away"} />
         </section>
+        <footer>
+          <button onClick={this.resetScore}>Reset Score</button>
+        </footer>
       </div>
     );
   }
