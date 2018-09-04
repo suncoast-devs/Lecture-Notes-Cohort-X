55ef8b956a61ec60224518cb5eb6aa2de41f8ffa
Mark Dewey <mark.t.dewey@gmail.com>
2018-09-04 12:12:11 -0400

    Added vue demo

--- intro-to-vue/src/App.vue
+++ intro-to-vue/src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WelcomeMessage userName="Jimbo" />
+    <Counter/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import WelcomeMessage from './components/WelcomeMessage.vue'
+import Counter from './components/Counter.vue'
+
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    WelcomeMessage, 
+    Counter
   }
 }
 </script>
@@ -25,4 +32,18 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+h1{
+  color:green;
+}
+
+img{
+ animation: img-spin infinite 20s linear;
+}
+
+@keyframes img-spin {
+  from{transform: rotate(0deg);}
+  to{transform: rotate(360);}
+}
+
 </style>
---
+++ intro-to-vue/src/components/WelcomeMessage.vue
@@ -0,0 +1,28 @@
+<template>
+    <section>
+        <h1>Greetings, {{userName}}</h1>
+
+        <input type="text" v-model="message">
+
+    </section>
+</template>
+
+<script>
+export default {
+    name:"WelcomeMessage", 
+    props: {
+        userName:String
+    }, 
+    data: function(){
+        return {
+            message:"hello"
+        }
+    }
+};
+</script>
+
+<style scoped>
+h1{
+    color:blue;
+}
+</style>
