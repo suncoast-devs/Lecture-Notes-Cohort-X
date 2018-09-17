a9f68c9285280a7a68fa94f076cd58da2dd76779
Mark Dewey <mark.t.dewey@gmail.com>
2018-09-17 13:13:02 -0400

    morning ORM notes

--- unit iii - back end engineering/intro-to-orms/Program.cs
+++ unit iii - back end engineering/intro-to-orms/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using intro_to_orms.Models;
 
 namespace intro_to_orms
 {
@@ -6,7 +8,41 @@ namespace intro_to_orms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // add board game, that the user names
+            // System.Console.WriteLine("Enter a Board Game to add:");
+            // var name = Console.ReadLine();
+
+            // var newgame = new BoardGames{
+            //     Name = name
+            // };
+
+            // var db = new HobbiesContext();
+
+            // db.BoardGamesTable.Add(newgame);
+
+            // db.SaveChanges();
+
+
+            // Get all Board games
+            // SQL : SELECT * FROM BoardGamesTable
+            var db = new HobbiesContext();
+
+            var games = db.BoardGamesTable;
+
+            foreach (var game in games)
+            {
+                System.Console.WriteLine($"game:{game.Name} ");
+            }
+
+            // Get all board games Where title = Risk
+            // SQL : SELECT * FROM BoardGamesTable WHERE Name = 'Risk'
+
+            var favoriteGame = db.BoardGamesTable
+                                .FirstOrDefault(game => game.Name == "Risk");
+
+            System.Console.WriteLine($"my favorite game is {favoriteGame.Name}");
+
+
         }
     }
 }
