adb4e26e5c9e54628b76c0645ef08f053831e560
Mark Dewey <mark.t.dewey@gmail.com>
2018-09-17 14:37:10 -0400

    all query examples

--- unit iii - back end engineering/intro-to-orms/Program.cs
+++ unit iii - back end engineering/intro-to-orms/Program.cs
@@ -23,25 +23,47 @@ namespace intro_to_orms
             // db.SaveChanges();
 
 
-            // Get all Board games
-            // SQL : SELECT * FROM BoardGamesTable
-            var db = new HobbiesContext();
+            // // Get all Board games
+            // // SQL : SELECT * FROM BoardGamesTable
+            // var db = new HobbiesContext();
 
-            var games = db.BoardGamesTable;
+            // var games = db.BoardGamesTable;
 
-            foreach (var game in games)
-            {
-                System.Console.WriteLine($"game:{game.Name} ");
-            }
+            // foreach (var game in games)
+            // {
+            //     System.Console.WriteLine($"game:{game.Name} ");
+            // }
 
-            // Get all board games Where title = Risk
-            // SQL : SELECT * FROM BoardGamesTable WHERE Name = 'Risk'
+            // // Get all board games Where title = Risk
+            // // SQL : SELECT * FROM BoardGamesTable WHERE Name = 'Risk'
 
-            var favoriteGame = db.BoardGamesTable
-                                .FirstOrDefault(game => game.Name == "Risk");
+            // var favoriteGame = db.BoardGamesTable
+            //                     .FirstOrDefault(game => game.Name == "Risk");
 
-            System.Console.WriteLine($"my favorite game is {favoriteGame.Name}");
+            // System.Console.WriteLine($"my favorite game is {favoriteGame.Name}");
 
+
+            // // Update
+
+            // // UPDATE BoardGamesTable SET [col] = value... WHERE ID = 3
+            // var gameToUpdate = db.BoardGamesTable.FirstOrDefault(game => game.Id == 1);
+
+            // gameToUpdate.MinimalAge = 9;
+            // gameToUpdate.NumberOfPlayers = 3;
+            // gameToUpdate.TimeLength = 90;
+            // gameToUpdate.Genre ="strategy";
+            // gameToUpdate.Difficulty = "easy";
+
+            // db.SaveChanges();
+
+
+            // // Delete
+
+            // var got = db.BoardGamesTable.FirstOrDefault(game => game.Name == "Game of Thrones");
+
+            // db.BoardGamesTable.Remove(got);
+
+            // db.SaveChanges();
 
         }
     }
