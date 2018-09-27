f1a19482b9e716f6cffd3f159d9e84737b24823d
Mark Dewey <mark.t.dewey@gmail.com>
2018-09-27 16:20:15 -0400

    added notes;

---
+++ unit iii - back end engineering/MultipleEndpoints/Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultipleEndpoints.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // PUT api/values/5
+        [HttpPatch("{id}/{value}")]
+        public ActionResult Vote(int id, string value)
+        {
+            if (value == "up"){
+                return Ok("Up");
+            } else if ( value == "down"){
+                return Ok("down");
+            } else {
+                return NotFound();
+            }
+        }
+
+        // [HttpPatch("{id}/down-vote")]
+        // public ActionResult Downvote(int id)
+        // {
+        //     return Ok("down");
+        // }
+    }
+}
--- unit iii - back end engineering/PlacesIHaveBeen/server/DataModel/Locations.cs
+++ unit iii - back end engineering/PlacesIHaveBeen/server/DataModel/Locations.cs
@@ -1,8 +1,10 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace PlacesTravelled.DataModel {
     public class Locations {
         public int Id { get; set; }
+        [Required]
         public string Place { get; set; }
         public DateTime Date { get; set; } = DateTime.Now;
 
